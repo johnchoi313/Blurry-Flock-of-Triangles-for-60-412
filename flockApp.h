4a217d4543bed9bfb1927f380b5d57e93f7b7f0b
John Choi <johnchoi313@gmail.com>
2015-02-11 23:53:39 -0500

    Create flockApp.h

---
+++ flockApp.h
@@ -0,0 +1,54 @@
+#ifndef _FLOCK_APP
+#define _FLOCK_APP
+
+// Includes
+#include "ofMain.h"
+#include "ofQuaternion.h"
+#include "ofxBlur.h"
+
+#define MAX_BOIDS                    30
+#define MAX_FLOCKS                    5
+
+// Forward declarations.
+class CBox;
+class CFlock;
+
+class flockApp : public ofBaseApp
+{
+  private:
+  CBox *box;
+  CFlock *Flocks[MAX_FLOCKS];
+
+  ofQuaternion wrld_rot;
+
+  int mouseDownX, mouseDownY;
+  bool inMouseDrag;
+  public:
+
+  CFlock *FlockAtIndex(int i)
+  {
+    if (i < 0 || i >= MAX_FLOCKS)
+      return NULL;
+
+    return Flocks[i];
+  }
+
+
+	public:
+		void setup();
+		void update();
+		void draw();
+		ofxBlur blur;
+    void exit();
+
+		void keyPressed  (int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+
+};
+
+#endif
